f74f4deb528b5f6266ace0f402c14da89a0c504e
crw243 <crweir@gmail.com>
2023-02-03 13:21:20 -0800

    new updates

--- bet.js
+++ bet.js
@@ -4,11 +4,38 @@ const { response } = require("express");
 
   
   
-  getPrint = ()=>{
-  fetch('http://localhost:3000/teams/api/')
-  .then((response) => response.json())
-  .then((data) => console.log(data.));  
-  }
+  // getPrint = ()=>{
+  // fetch('http://localhost:3000/teams/api/')
+  // .then((response) => response.json())
+  // .then((data) => console.log(data.));  
+  // }
+  // getPrint()
 
-  getPrint()
+  // const teamJoin = `SELECT *
+// FROM games JOIN teams t1 ON games.team1 = t1.teamID 
+// JOIN teams t2 games.team2 = t2.teamID  ;`
 
+
+// joinCall = ()=>{
+// db.all(teamJoin, [], (err, rows) => {
+//   if (err) {
+//     throw err;
+//   }
+//   rows.forEach((row) => {
+//     console.log(row.team1SpreadOdds)
+ 
+//   });
+// });
+// }
+
+// joinCall()
+
+// app.get("/teams/api", (req, res) => {
+//   const sql = "SELECT * FROM teams ORDER BY teamID";
+//   db.all(sql, [], (err, rows) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     res.send({rows});
+//   });
+// });
