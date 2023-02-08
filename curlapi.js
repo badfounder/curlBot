873ea804478b10254f7ff48bfcd4e4b6cd14f71d
crw243 <crweir@gmail.com>
2023-02-08 05:21:17 -0800

    update/  Please enter the commit message for your changes. Lines starting

--- curlapi.js
+++ curlapi.js
@@ -1,25 +1,37 @@
+const db = require("./data/db_utils.js")
+const sqlite3 = require("sqlite3").verbose();
+const sqlite = require('sqlite')
 
-const teamData = 
+const sql = "INSERT OR IGNORE INTO teams (teamID,ccID) VALUES (?,?)";
+const params = [team.name, team.id]
+
+async function getTeams () {
 fetch('https://legacy-curlingio.global.ssl.fastly.net/api/organizations/qw4LUsJ1_aQ/competitions/8079/teams')
-  .then((response) => response.json())
-  .then((data) => { 
-   let teams = []
-    for (const team of data)
-    teams.push(team.short_name)
-    console.log(teams)
-   
-  });
+const response = await fetch('https://legacy-curlingio.global.ssl.fastly.net/api/organizations/qw4LUsJ1_aQ/competitions/8079/teams')
+const teams = await response.json();
+return teams;
 
- 
+};
 
-  const gameData = 
-fetch('https://legacy-curlingio.global.ssl.fastly.net/api/organizations/qw4LUsJ1_aQ/competitions/8079/games')
-  .then((response) => response.json())
-  .then((data) => { 
-   let games = []
-    for (const game of data)
-    console.log(`${game.name},${game.draw.label},${game.id},${game.draw.starts_at}`)
-  });
+getTeams().then( fetch( ))
+  
+})
+
+
+
+
+
+    
+    
+
+//   const gameData = 
+// fetch('https://legacy-curlingio.global.ssl.fastly.net/api/organizations/qw4LUsJ1_aQ/competitions/8079/games')
+//   .then((response) => response.json())
+//   .then((data) => { 
+//    let games = []
+//     for (const game of data)
+//     console.log(`${game.name},${game.draw.label},${game.id},${game.draw.starts_at},${game.game_positions[0].competition_team_id},${game.game_positions[1].competition_team_id}`)
+//   });
 
 
   
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const path = require("path");
 const sqlite3 = require("sqlite3").verbose();
 var bodyParser = require('body-parser');
 const { json } = require("body-parser");
+const db = require("./data/db_utils.js") //brings in data base mgmt module
+
 const bankRoll = 1000
 
 //Express Server Creation
@@ -15,100 +17,22 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json({ type: 'application/*+json' }))
 
-var db = require("./db_utils.js")
-
-
-// Création de la table Livres (Livre_ID, Titre, Auteur, Commentaires)
-const sql_create = `CREATE TABLE IF NOT EXISTS teams ( 
-    "id" INTEGER PRIMARY KEY,
-    "teamID" VARCHAR UNIQUE, 
-    "win" REAL, 
-    "gender" TEXT,
-    "lose" REAL, 
-    "winPer" REAL as (ROUND("win"/("win"+"lose"),2)), 
-    "ptsFor" REAL, 
-    "ptsAgainst" REAL, 
-    "netScore" REAL as (ROUND("ptsFor" - "ptsAgainst",2)), 
-    "hamEff" REAL,
-    "stlDef" REAL,
-    "netEff" REAL as (ROUND("hamEff" - "stlDef",2)) 
-);`;
-
-db.run(sql_create, err => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Creation of Table 'teams'");
-  
- //Create a second table for the games.
- 
-  const sql_create2 = `
-  CREATE TABLE IF NOT EXISTS games ( 
-    "id" INTEGER PRIMARY KEY,
-    "gameID" VARCHAR UNIQUE, 
-    "tournament" INT,
-    "draw" TEXT, 
-    "date" TEXT, 
-    "team1" TEXT, 
-    "team2" TEXT, 
-    "ovUnd" REAL, 
-    "ovUndLine" REAL, 
-    "undLine" REAL,
-    "team1StraightOdds" REAL,
-    "team2StraightOdds" REAL, 
-    "team1SpreadOdds" REAL,
-    "team2SpreadOdds" REAL,
-    "ptsSprd" REAL,
-    "ccUUID" TEXT,
-    "gender" TEXT
-  )`;
-
-db.run(sql_create2, err => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log("Creation of Table 'games'");
-})
-  
-  // Seed Data in The Table.
-  const sql_insert = `INSERT OR IGNORE INTO teams (teamID,win,lose,ptsFor,ptsAgainst,hamEff,stlDef) VALUES
-  ("Edin",61,13,8.55,5.42,0.48,0.15),
-  ("Gim",54,20,8.83,5.72,0.51,0.14),
-  ("Constantini",34,20,8.08,5.99,0.4,0.25)
-  ;`;
-  db.run(sql_insert, err => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log("Seeded Data into Teams Table");
-  });
-});
-
-  // feed the games table
-  const sql_insert = `INSERT OR IGNORE INTO games (gameID,tournament,date,team1,team2,ovUnd,ovUndLine,team1StraightOdds,team2StraightOdds,team1SpreadOdds,team2SpreadOdds,ptsSprd) VALUES
-  ("game1","CADOPEN23","10/01/2023","Gim","Constantini","10.5","1.72","1.3","3.2","1.65","2.1","-1.5")
-  ;`
-
-  db.run(sql_insert, err => {
-    if (err) {
-      return console.error(err.message);
-    }
-    console.log("Seeded Data into Games Table");
-  });
-
-
-
 // Launch the server
 app.listen(3000, () => {
-    console.log("Server started (http://localhost:3000/) !");
-  });
+  console.log("Server started (http://localhost:3000/) !");
+});
 
+
+//calling functions to create SQL Tables
+db.ctt //Creates the table teams and seeds it with data. (Function from db_utils)
+db.cgt //Creates a second table for games and seeds its with data.
+ 
+ 
 // GET /
 app.get("/", (req, res) => {
   // res.send("Bonjour le monde...");
   res.render("index");
 });
-
 
 // GET /teams
 app.get("/teams", (req, res) => {
